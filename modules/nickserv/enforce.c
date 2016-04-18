e2bc4e39dd73292d306a3683d2c1d02313e49a15
Ben C <ben@chatlounge.net>
2016-04-18 01:29:53 -0500

    nickserv/enforce: Use base36_encode in enforce.  Instead of 100,000 potential guest nicks, there's now 1,679,616.  And, guest nicks are now one character shorter.

--- modules/nickserv/enforce.c
+++ modules/nickserv/enforce.c
@@ -100,7 +100,8 @@ static void guest_nickname(user_t *u)
 	 * you shouldn't use this module. */
 	for (tries = 0; tries < 30; tries++)
 	{
-		snprintf(gnick, sizeof gnick, "%s%d", nicksvs.enforce_prefix, arc4random()%100000);
+		snprintf(gnick, sizeof gnick, "%s%s", nicksvs.enforce_prefix, base36_encode(arc4random()%1679616));
+		/* 36^4 = 1,679,616 */
 
 		if (!user_find_named(gnick))
 			break;
