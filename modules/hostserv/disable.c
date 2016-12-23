e20842982bd1d7a50faed6dc7855dfba39666884
Ben C <ben@chatlounge.net>
2016-12-23 01:13:11 -0600

    hostserv/disable: Make it work regardless of current nick at the time.

--- modules/hostserv/disable.c
+++ modules/hostserv/disable.c
@@ -1,18 +1,18 @@
 /*
- *	Description:
+ *  Description:
  *
- *		Permits a user to disable his vhost setting permanently, without oper
- *	intervention.
+ *      Permits a user to disable his vhost setting permanently, without oper
+ *  intervention.
  *
- *		Some code borrowed from vhost.c.
+ *      Some code borrowed from vhost.c.
  *
  *  Rationale:
  *
- *		/HostServ OFF only disables the VHOST for the duration of IRC session
- *	and must be run at the beginning of every IRC session, prior to oper
- *	intervention.
+ *      /HostServ OFF only disables the VHOST for the duration of IRC session
+ *  and must be run at the beginning of every IRC session, prior to oper
+ *  intervention.
  *
- *	Copyright (c) 2015-2016 - ChatLounge IRC Network Development Team
+ *  Copyright (c) 2015-2016 - ChatLounge IRC Network Development Team
  */
  
 #include "atheme.h"
@@ -41,10 +41,14 @@ void _moddeinit(module_unload_intent_t intent)
 
 static void hs_cmd_disable(sourceinfo_t *si, int parc, char *parv[])
 {
-	myuser_t *mu = myuser_find_by_nick(si->su->nick);
-	
-	hs_sethost_all(mu, NULL, get_source_name(si));
-	command_success_nodata(si, _("Deleted all vhosts for \2%s\2."), entity(mu)->name);
-	logcommand(si, CMDLOG_ADMIN, "VHOST:REMOVE: \2%s\2", entity(mu)->name);
-	do_sethost_all(mu, NULL);
+	if (si->smu == NULL)
+	{
+		command_fail(si, fault_noprivs, _("You are not logged in."));
+		return;
+	}
+
+	hs_sethost_all(si->smu, NULL, get_source_name(si));
+	command_success_nodata(si, _("Deleted all vhosts for \2%s\2."), entity(si->smu)->name);
+	logcommand(si, CMDLOG_ADMIN, "VHOST:REMOVE: \2%s\2", entity(si->smu)->name);
+	do_sethost_all(si->smu, NULL);
 }
