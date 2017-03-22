b1aba7b27e5734b643c5729a37c99b571fc0480c
Ben C <ben@chatlounge.net>
2017-03-22 03:43:22 -0500

    chanserv/set_email: Add HISTORY support.

--- modules/chanserv/set_email.c
+++ modules/chanserv/set_email.c
@@ -1,6 +1,8 @@
 /*
  * Copyright (c) 2003-2004 E. Will et al.
  * Copyright (c) 2006-2010 Atheme Development Group
+ * Copyright (c) 2017 ChatLounge IRC Network Development Team
+ *
  * Rights to this code are documented in doc/LICENSE.
  *
  * This file contains routines to handle the CService SET EMAIL command.
@@ -13,8 +15,10 @@ DECLARE_MODULE_V1
 (
 	"chanserv/set_email", false, _modinit, _moddeinit,
 	PACKAGE_STRING,
-	"Atheme Development Group <http://www.atheme.org>"
+	"ChatLounge IRC Network Development Team <http://www.chatlounge.net>"
 );
+
+void (*add_history_entry)(sourceinfo_t *si, mychan_t *mc, const char *desc) = NULL;
 
 static void cs_cmd_set_email(sourceinfo_t *si, int parc, char *parv[]);
 
@@ -25,6 +29,9 @@ mowgli_patricia_t **cs_set_cmdtree;
 void _modinit(module_t *m)
 {
 	MODULE_TRY_REQUEST_SYMBOL(m, cs_set_cmdtree, "chanserv/set_core", "cs_set_cmdtree");
+
+	if (module_locate_symbol("chanserv/history", "add_history_entry"))
+		add_history_entry = module_locate_symbol("chanserv/history", "add_history_entry");
 
 	command_add(&cs_set_email, *cs_set_cmdtree);
 }
@@ -75,7 +82,21 @@ static void cs_cmd_set_email(sourceinfo_t *si, int parc, char *parv[])
 	metadata_add(mc, "email", mail);
 
 	logcommand(si, CMDLOG_SET, "SET:EMAIL: \2%s\2 \2%s\2", mc->name, mail);
-	command_success_nodata(si, _("The e-mail address for channel \2%s\2 has been set to \2%s\2."), parv[0], mail);
+	command_success_nodata(si, _("The e-mail address for channel \2%s\2 has been set to: \2%s\2"), parv[0], mail);
+
+	if (add_history_entry == NULL)
+	{
+		add_history_entry = module_locate_symbol("chanserv/history", "add_history_entry");
+	}
+
+	if (add_history_entry != NULL)
+	{
+		char desc[350];
+
+		snprintf(desc, sizeof desc, "E-mail address set to: %s", mail);
+
+		add_history_entry(si, mc, desc);
+	}
 }
 
 /* vim:cinoptions=>s,e0,n0,f0,{0,}0,^0,=s,ps,t0,c3,+s,(2s,us,)20,*30,gs,hs
