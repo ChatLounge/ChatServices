e3bd26d8be0a0d47fd7672931a580bc2c06d92cd
Ben C <ben@chatlounge.net>
2017-07-16 03:06:52 -0500

    GroupServ/list: Make LIST show the group flags of each group.

--- modules/groupserv/list.c
+++ modules/groupserv/list.c
@@ -1,5 +1,7 @@
 /*
  * Copyright (c) 2005 Atheme Development Group
+ * Copyright (c) 2017 ChatLounge IRC Network Development Team
+ *
  * Rights to this code are documented in doc/LICENSE.
  *
  * This file contains routines to handle the GroupServ HELP command.
@@ -13,7 +15,7 @@ DECLARE_MODULE_V1
 (
 	"groupserv/list", false, _modinit, _moddeinit,
 	PACKAGE_STRING,
-	"Atheme Development Group <http://www.atheme.org>"
+	"ChatLounge IRC Network Development Team <http://www.chatlounge.net/>"
 );
 
 static void gs_cmd_list(sourceinfo_t *si, int parc, char *parv[]);
@@ -27,6 +29,7 @@ static void gs_cmd_list(sourceinfo_t *si, int parc, char *parv[])
 	char *pattern = parv[0];
 	unsigned int matches = 0;
 	myentity_iteration_state_t state;
+	char buf[BUFSIZE];
 
 	if (!pattern)
 	{
@@ -38,6 +41,8 @@ static void gs_cmd_list(sourceinfo_t *si, int parc, char *parv[])
 	/* No need to say "Groups currently registered". You can't have a unregistered group. */
 	command_success_nodata(si, _("Groups matching pattern \2%s\2:"), pattern);
 
+	*buf = '\0';
+
 	MYENTITY_FOREACH_T(mt, &state, ENT_GROUP)
 	{
 		mygroup_t *mg = group(mt);
@@ -46,7 +51,43 @@ static void gs_cmd_list(sourceinfo_t *si, int parc, char *parv[])
 
 		if (!match(pattern, entity(mg)->name))
 		{
-			command_success_nodata(si, _("- %s (%s)"), entity(mg)->name, mygroup_founder_names(mg));
+			if (mg->flags & MG_REGNOLIMIT)
+				mowgli_strlcat(buf, "REGNOLIMIT", BUFSIZE);
+
+			if (mg->flags & MG_ACSNOLIMIT)
+			{
+				if (*buf)
+					mowgli_strlcat(buf, " ", BUFSIZE);
+
+				mowgli_strlcat(buf, "ACSNOLIMIT", BUFSIZE);
+			}
+
+			if (mg->flags & MG_OPEN)
+			{
+				if (*buf)
+					mowgli_strlcat(buf, " ", BUFSIZE);
+
+				mowgli_strlcat(buf, "OPEN", BUFSIZE);
+			}
+
+			if (mg->flags & MG_PUBACL)
+			{
+				if (*buf)
+					mowgli_strlcat(buf, " ", BUFSIZE);
+
+				mowgli_strlcat(buf, "PUBACL", BUFSIZE);
+			}
+
+			if (mg->flags & MG_PUBLIC)
+			{
+				if (*buf)
+					mowgli_strlcat(buf, " ", BUFSIZE);
+
+				mowgli_strlcat(buf, "PUBLIC", BUFSIZE);
+			}
+
+			command_success_nodata(si, _("- %s (%s) (Flags: %s)"), entity(mg)->name, mygroup_founder_names(mg),
+				*buf ? buf : "<None>");
 			matches++;
 		}
 	}
@@ -56,7 +97,7 @@ static void gs_cmd_list(sourceinfo_t *si, int parc, char *parv[])
 	else
 		command_success_nodata(si, ngettext(N_("\2%d\2 match for pattern \2%s\2"), N_("\2%d\2 matches for pattern \2%s\2"), matches), matches, pattern);
 
-	logcommand(si, CMDLOG_ADMIN, "LIST: \2%s\2 (\2%d\2 matches)", pattern, matches);
+	logcommand(si, CMDLOG_ADMIN, "LIST: \2%s\2 (\2%d\2 match%s)", pattern, matches, matches == 1 ? "" : "es");
 }
 
 void _modinit(module_t *m)
