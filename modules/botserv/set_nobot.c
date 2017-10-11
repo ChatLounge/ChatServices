f90cc928170b7c60811e011fc0b1fbf3400c0df0
Ben C <ben@chatlounge.net>
2017-10-11 03:49:38 -0500

    BotServ/set_nobot: Add some additional checks, as well as HISTORY and VERBOSE support.

--- modules/botserv/set_nobot.c
+++ modules/botserv/set_nobot.c
@@ -19,6 +19,9 @@ DECLARE_MODULE_V1
 	"ChatLounge IRC Network Development Team <https://www.chatlounge.net>"
 );
 
+void (*notify_channel_set_change)(sourceinfo_t *si, myuser_t *tmu, mychan_t *mc,
+	const char *settingname, const char *setting) = NULL;
+
 mowgli_patricia_t **bs_set_cmdtree;
 
 static void bs_cmd_set_nobot(sourceinfo_t *si, int parc, char *parv[]);
@@ -28,6 +31,9 @@ command_t bs_set_nobot = { "NOBOT", N_("Prevent a bot from being assigned to a c
 void _modinit(module_t *m)
 {
 	MODULE_TRY_REQUEST_SYMBOL(m, bs_set_cmdtree, "botserv/set_core", "bs_set_cmdtree");
+
+	if (module_request("chanserv/main"))
+		notify_channel_set_change = module_locate_symbol("chanserv/main", "notify_channel_set_change");
 
 	command_add(&bs_set_nobot, *bs_set_cmdtree);
 }
@@ -72,6 +78,12 @@ static void bs_cmd_set_nobot(sourceinfo_t *si, int parc, char *parv[])
 
 	if (!irccasecmp(option, "ON") || !irccasecmp(option, "1") || !irccasecmp(option, "TRUE"))
 	{
+		if (metadata_find(mc, "private:botserv:no-bot"))
+		{
+			command_fail(si, fault_nochange, _("The \2%s\2 flag is already set for channel: \2%s\2"), "NOBOT", mc->name);
+			return;
+		}
+
 		metadata_add(mc, "private:botserv:no-bot", "ON");
 		if ((md = metadata_find(mc, "private:botserv:bot-assigned")) != NULL)
 		{
@@ -84,14 +96,43 @@ static void bs_cmd_set_nobot(sourceinfo_t *si, int parc, char *parv[])
 			metadata_delete(mc, "private:botserv:bot-assigned");
 			metadata_delete(mc, "private:botserv:bot-handle-fantasy");
 		}
+
+		if (!metadata_find(mc, "disable_fantasy"))
+		{
+			metadata_add(mc, "disable_fantasy", "ON");
+			logcommand(si, CMDLOG_SET, "SET:FANTASY:OFF: \2%s\2", mc->name);
+			verbose(mc, _("\2%s\2 disabled the FANTASY flag."), get_source_name(si));
+			command_success_nodata(si, _("The \2%s\2 flag has been removed for channel \2%s\2."), "FANTASY", mc->name);
+			notify_channel_set_change(si, si->smu, mc, "FANTASY", "OFF");
+		}
+
+		if (metadata_find(mc, "private:botserv:saycaller"))
+		{
+			metadata_delete(mc, "private:botserv:saycaller");
+			logcommand(si, CMDLOG_SET, "SET:SAYCALLER:OFF: \2%s\2", mc->name);
+			command_success_nodata(si, _("Say Caller is now \2OFF\2 on channel %s."), mc->name);
+			verbose(mc, _("\2%s\2 disabled the SAYCALLER flag."), get_source_name(si));
+			notify_channel_set_change(si, si->smu, mc, "SAYCALLER", "OFF");
+		}
+
 		logcommand(si, CMDLOG_SET, "SET:NOBOT:ON: \2%s\2", mc->name);
+		verbose(mc, _("\2%s\2 enabled the NOBOT flag."), get_source_name(si));
 		command_success_nodata(si, _("No Bot mode is now \2ON\2 on channel %s."), mc->name);
+		notify_channel_set_change(si, si->smu, mc, "NOBOT", "ON");
 	}
 	else if(!irccasecmp(option, "OFF") || !irccasecmp(option, "0") || !irccasecmp(option, "FALSE"))
 	{
+		if (!metadata_find(mc, "private:botserv:no-bot"))
+		{
+			command_fail(si, fault_nochange, _("The \2%s\2 flag is not set for channel: \2%s\2"), "NOBOT", mc->name);
+			return;
+		}
+
 		metadata_delete(mc, "private:botserv:no-bot");
 		logcommand(si, CMDLOG_SET, "SET:NOBOT:OFF: \2%s\2", mc->name);
+		verbose(mc, _("\2%s\2 disabled the NOBOT flag."), get_source_name(si));
 		command_success_nodata(si, _("No Bot mode is now \2OFF\2 on channel %s."), mc->name);
+		notify_channel_set_change(si, si->smu, mc, "NOBOT", "OFF");
 	}
 	else
 	{
